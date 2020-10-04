a3293733eef0cc627a6206386531d9589f4d0828
Jonathan Pinto <pintojonathan11@gmail.com>
2020-10-04 00:23:05 -0400

    Added Experiences

--- personal-website/src/components/Experience/Experience.js
+++ personal-website/src/components/Experience/Experience.js
@@ -4,6 +4,59 @@ import { Container, Row, Col } from "react-bootstrap";
 import SpecificExperience from "./SpecificExperience";
 
 function Experience() {
+  var amazonLanguagesUsed = [
+    [
+      "AWS Services",
+      "Lambda",
+      "DynamoDB",
+      "Batch",
+      "CloudWatch",
+      "S3",
+      "IAM",
+      "EMR"
+    ],
+    ["Programming Languages", "Java", "Python"]
+  ];
+  var morganLanguagesUsed = [
+    ["Frontend", "Angular8", "Bootstrap4"],
+    ["Backend", "Spring", "Apache CXF", "Java"]
+  ];
+  var arestyLanguagesUsed = [
+    ["Languages and Tools", "Unity", "C#", "KADAPT package"]
+  ];
+
+  var amazonExtraInfo = [
+    "Contributed to a large defect detection codebase by building a batch pipeline for implementing a scalable aggregation feature which was programmed mostly in Java.",
+    "Expedited the process of surfacing, tracking, and solving defects by weeks.",
+    "Participated in daily standups and weekly sprints.",
+    "Generated multiple code reviews and wikis.",
+    "Developed an additional side project using the Alexa Skills SDK for Python.",
+    "Presented two design reviews for the team."
+  ];
+  var morganExtraInfo = [
+    "Built a machine learning-as-a-service platform that allows users to train algorithms, build models, and test prebuilt and saved models.",
+    "Saved internal teams weeks of unnecessary communication among different teams at different levels.",
+    "Used by Developer, Business, QA, and Operation teams",
+    "Presented my project three times to managing directors."
+  ];
+  var arestyExtraInfo = [
+    "Developed problem-solving skills in the research environment and learned to communicate research to diverse audiences.",
+    "Develop programs in Unity to simulate crowds (e.g., virtual characters) in the virtual environments.",
+    "Explored different evaluation measures that evaluate the proclivity of virtual environments for occupants to maintain a safe distance in relation to the overall occupancy rate.",
+    "Worked towards the development of a framework (in Unity) consisting of multiple levels controlling behavioral policies of virtual crowds.",
+    "Evaluated the measures on the designed framework for different virtual environments as case studies."
+  ];
+
+  var odasisExtraInfo = [
+    "Made lesson plan and taught weekly sessions for Calculus I",
+    "Administered quizzes and aided students in exam-taking skills.",
+    "Provided resources and other pertinent information to first-year students that enabled them to excel in Calculus and at Rutgers University."
+  ];
+
+  var jeiExtraInfo = [
+    "Taught different math subjects that range from adding to calculus II"
+  ];
+
   return (
     <div id="experience" className="experience-screen">
       <Container fluid>
@@ -20,6 +73,8 @@ function Experience() {
               position="Software Development Engineer Intern"
               date="(June 2020 - August 2020)"
               description="Backend Project: Built a scalable aggregation feature into a large defect detection codebase using various AWS services and Java"
+              languagesUsed={amazonLanguagesUsed}
+              extraInfo={amazonExtraInfo}
             ></SpecificExperience>
           </Col>
           <Col lg="4" md="8" xs="15" className="mt-5 mr-sm-5 ml-sm-5">
@@ -29,6 +84,8 @@ function Experience() {
               position="Technology Summer Analyst Intern"
               date="(June 2019 - August 2019)"
               description="Full-Stack Project: Built a machine learning-as-a-service webapp that allowed internal teams to efficiently create ML models by providing the appropriate data"
+              languagesUsed={morganLanguagesUsed}
+              extraInfo={morganExtraInfo}
             ></SpecificExperience>
           </Col>
           <Col lg="4" md="8" xs="15" className="mt-5 mr-sm-5 ml-sm-5">
@@ -38,6 +95,8 @@ function Experience() {
               position="Research Assistant"
               date="(Septembeer 2020 - May 2021)"
               description="Explored different evaluation measures that evaluate social distancing and developed a framework in Unity"
+              languagesUsed={arestyLanguagesUsed}
+              extraInfo={arestyExtraInfo}
             ></SpecificExperience>
           </Col>
           <Col lg="4" md="8" xs="15" className="mt-5 mr-sm-5 ml-sm-5">
@@ -47,6 +106,7 @@ function Experience() {
               position="Supplemental Instructor for Calculus I"
               date="(September 2017 - May 2019)"
               description="Taught a Calculus I course for students who come from a low socio-economic background"
+              extraInfo={odasisExtraInfo}
             ></SpecificExperience>
           </Col>
           <Col
@@ -61,6 +121,7 @@ function Experience() {
               position="Math Tutor"
               date="(August 2018 - May 2019)"
               description="Taught Math to children from the ages of 5 to 18"
+              extraInfo={jeiExtraInfo}
             ></SpecificExperience>
           </Col>
         </Row>
--- personal-website/src/components/Experience/SpecificExperience.js
+++ personal-website/src/components/Experience/SpecificExperience.js
@@ -5,10 +5,138 @@ import {
   MDBCardBody,
   MDBCardImage,
   MDBCardTitle,
-  MDBCardText
+  MDBCardText,
+  MDBCollapse,
+  MDBCol,
+  MDBRow,
+  MDBIcon
 } from "mdbreact";
 
 import Divider from "@material-ui/core/Divider";
+import { Modal, Button, Dropdown, ListGroup, Collapse } from "react-bootstrap";
+
+function MyVerticallyCenteredModal(props) {
+  var company = props.company;
+  var position = props.position;
+  var languagesUsed = props.languagesUsed;
+  var extraInfo = props.extraInfo;
+
+  const [open0, setOpen0] = React.useState(false);
+  const [open1, setOpen1] = React.useState(false);
+
+  const techGroup0Header =
+    languagesUsed != null ? (
+      <ListGroup.Item
+        className="card-text-title card-text-description-title"
+        onClick={() => setOpen0(!open0)}
+      >
+        <MDBRow>
+          <MDBCol>{languagesUsed[0][0]}</MDBCol>
+          <div className="d-flex justify-content-end mr-3">
+            <MDBIcon
+              className={open0 ? "rotate" : "rotate-back"}
+              icon="arrow-down"
+            />
+          </div>
+        </MDBRow>
+      </ListGroup.Item>
+    ) : null;
+
+  const techGroup1Header =
+    languagesUsed != null && languagesUsed.length >= 2 ? (
+      <ListGroup.Item
+        className="card-text-title card-text-description-title"
+        onClick={() => setOpen1(!open1)}
+      >
+        <MDBRow>
+          <MDBCol>{languagesUsed[1][0]}</MDBCol>
+          <div className="d-flex justify-content-end mr-3">
+            <MDBIcon
+              className={open1 ? "rotate" : "rotate-back"}
+              icon="arrow-down"
+            />
+          </div>
+        </MDBRow>
+      </ListGroup.Item>
+    ) : null;
+
+  const techGroup0 =
+    languagesUsed != null
+      ? languagesUsed[0]
+          .slice(1)
+          .map(str => <ListGroup.Item>{str}</ListGroup.Item>)
+      : null;
+
+  const techGroup1 =
+    languagesUsed != null && languagesUsed.length >= 2
+      ? languagesUsed[1]
+          .slice(1)
+          .map(str => <ListGroup.Item>{str}</ListGroup.Item>)
+      : null;
+
+  const listDescriptionItems = extraInfo.map(str => (
+    <ul>
+      {" "}
+      <li className="li">{str}</li>{" "}
+    </ul>
+  ));
+
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          <div className="title card-text-title">
+            {position} at {company}
+          </div>
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4 className="mb-3 card-text-description-title">Description</h4>
+        <div>{listDescriptionItems}</div>
+      </Modal.Body>
+      {languagesUsed != null && (
+        <div>
+          <Dropdown.Divider />
+          <Modal.Body>
+            <h4 className="mb-3 card-text-description-title">
+              Technologies Used
+            </h4>
+            <div>
+              <ListGroup>
+                <div className="mb-3">
+                  {techGroup0Header != null && techGroup0Header}
+                  {techGroup0 != null && (
+                    <MDBCollapse id="basicCollapse" isOpen={open0}>
+                      {techGroup0}
+                    </MDBCollapse>
+                  )}
+                </div>
+                <div className="mb-3">
+                  {techGroup1Header != null && techGroup1Header}
+                  {techGroup1 != null && (
+                    <MDBCollapse id="basicCollapse" isOpen={open1}>
+                      {techGroup1}
+                    </MDBCollapse>
+                  )}
+                </div>
+              </ListGroup>
+            </div>
+          </Modal.Body>
+        </div>
+      )}
+      <Modal.Footer>
+        <MDBBtn color="mdb-color" onClick={props.onHide}>
+          Close
+        </MDBBtn>
+      </Modal.Footer>
+    </Modal>
+  );
+}
 
 function SpecificExperience(props) {
   var image = props.image;
@@ -16,6 +144,10 @@ function SpecificExperience(props) {
   var position = props.position;
   var date = props.date;
   var description = props.description;
+  var languagesUsed = props.languagesUsed;
+  var extraInfo = props.extraInfo;
+
+  const [modalShow, setModalShow] = React.useState(false);
 
   return (
     <div className="">
@@ -29,12 +161,30 @@ function SpecificExperience(props) {
 
           <MDBCardTitle className="position">{position}</MDBCardTitle>
           <MDBCardTitle className="date">{date}</MDBCardTitle>
-          <MDBCardTitle className="description ml-3 mr-3">
+          <MDBCardTitle className="description ml-3 mr-3 mb-3">
             {description}
           </MDBCardTitle>
-          <MDBBtn color="mdb-color">Learn More</MDBBtn>
+          {extraInfo != null && (
+            <MDBBtn
+              className="mb-3"
+              color="mdb-color"
+              onClick={() => setModalShow(true)}
+            >
+              Learn More
+            </MDBBtn>
+          )}
         </MDBCardBody>
       </MDBCard>
+      {extraInfo != null && (
+        <MyVerticallyCenteredModal
+          show={modalShow}
+          onHide={() => setModalShow(false)}
+          company={company}
+          position={position}
+          languagesUsed={languagesUsed}
+          extraInfo={extraInfo}
+        />
+      )}
     </div>
   );
 }
